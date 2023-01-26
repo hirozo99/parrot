93fd72c3dcdfffaa52f4bd198280703576d574db
katohiro <hiro.1999@me.com>
2023-01-26 22:32:16 +0900

    add

--- test.py
+++ test.py
@@ -11,52 +11,61 @@ import numpy as np
 from olympe.video.pdraw import Pdraw, PdrawState
 from olympe.video.renderer import PdrawRenderer
 
+# parrot設定
 RTSP_URL = 'rtsp://192.168.42.1/live'
 os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
-
 DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
 
-### --- aruco設定 --- ###
+# aruco設定
 dict_aruco = aruco.Dictionary_get(aruco.DICT_4X4_50)
 parameters = aruco.DetectorParameters_create()
 
+# 変数の設定
 target_found = False
 start_processing = False
 
-
-def test_takeoff(drone):
-    print("--------------------test_takeoff--------------------")
+# 離陸
+def takeoff(drone):
+    print("------------------------------takeoff------------------------------")
     assert drone(TakeOff()).wait().success()
+    time.sleep(5)
+
+# 毎秒0.7mでFm前進後、3秒ポーズ
+def go(drone, F):
+    print("------------------------------forward------------------------------")
+    assert drone(
+        extended_move_by(F, 0, 0, 0, 0.7, 0.7, 0.7)
+    ).wait().success()
     time.sleep(3)
 
-
-def go(distance):
-    os.system("python3 go.py -m {}".format(distance))
-
-
-def height(z):
-    os.system("python3 height.py -m {}".format(z))
-
-
-def test_move(drone, F, H):
-    print("--------------------test_move--------------------")
+# 毎秒0.2mでFm前進
+def forward(drone, F):
+    print("------------------------------forward------------------------------")
     assert drone(
-        extended_move_by(F, 0, -H, 0, 0.7, 0.7, 0.7)
+        extended_move_by(F, 0, 0, 0, 0.2, 0.2, 0.2)
     ).wait().success()
 
+# 毎秒0.7mでHm高度上昇
+def gain_altitude(drone, H):
+    print("------------------------------gain_altitude------------------------------")
+    assert drone(
+        extended_move_by(0, 0, -H, 0, 0.7, 0.7, 0.7)
+    ).wait().success()
+    time.sleep(3)
 
-def test_landing(drone):
-    print("--------------------test_landing--------------------")
+# 着陸
+def landing(drone):
+    print("------------------------------landing------------------------------")
     drone(Landing()).wait().success()
     drone.disconnect()
 
-
+#
 def frame_processing(frame):
     if not start_processing:
         return
     global target_found
     try:
-        # convert frame to rgb'
+        # convert frame to rgb
         cv2_convert_flag = {
             olympe.VDEF_I420: cv2.COLOR_YUV2BGR_I420,
             olympe.VDEF_NV12: cv2.COLOR_YUV2BGR_NV12
@@ -70,7 +79,23 @@ def frame_processing(frame):
         print(list_ids)
 
         if list_ids[0] == 0:
-            print("着陸体制に入ります！！")
+            print("*********************************************************************")
+            print("***************************landing posture***************************")
+            print("*********************************************************************")
+
+            index = np.where(ids == 0)[0][0]  # num_id が格納されているindexを抽出
+            cornerUL = corners[index][0][0]
+            cornerUR = corners[index][0][1]
+            cornerBR = corners[index][0][2]
+            cornerBL = corners[index][0][3]
+
+            center = [(cornerUL[0] + cornerBR[0]) / 2, (cornerUL[1] + cornerBR[1]) / 2]
+
+            # print('左上 : {}'.format(cornerUL))
+            # print('右上 : {}'.format(cornerUR))
+            # print('右下 : {}'.format(cornerBR))
+            # print('左下 : {}'.format(cornerBL))
+            print('中心 : {}'.format(center))
             # test_landing(drone)
             if list_ids[-1] == 4 and len(list_ids) == 5:
                 print("--------------------------着陸--------------------------")
@@ -85,31 +110,31 @@ def frame_processing(frame):
 
 
 if __name__ == '__main__':
-    # connec drone
+    # ドローン接続
     drone = olympe.Drone(DRONE_IP)
     drone.connect()
 
+    # カメラ起動
     pdraw = Pdraw()
     pdraw.set_callbacks(raw_cb=frame_processing)
-
     pdraw.play(url=RTSP_URL)
 
     assert pdraw.wait(PdrawState.Playing, timeout=5)
-    print('doe initialize')
+    print("**********************start**********************")
 
-    test_takeoff(drone)
+    takeoff(drone)
     time.sleep(1)
-    test_move(drone, 0, 2)
+    gain_altitude(drone, 2)
     time.sleep(1)
     start_processing = True
     try:
         while True:
             if target_found:
                 start_processing = False
-                test_move(drone, 1, 0)
+                go(drone, 1)
                 time.sleep(1)
-                test_landing(drone)
+                landing(drone)
                 break
     except Exception as e:
         print(e)
-        test_landing(drone)
+        landing(drone)
